1c0f3c2c9db228205ff8249ee8ebf0b9734b6dfe
Visioli-emanuele <visioli.emanuele@iisgalvanimi.edu.it>
2023-01-10 05:45:51 +0000

    3° file, docker ps manca il cntainer di mongo

--- server/app.py
+++ server/app.py
@@ -1,10 +1,17 @@
+
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def ping_server():
-    return "Welcome to the world of animals."
+    return "Welcome to the world of animals....yeah"
+
+@app.route('/simple_json')
+def simple_json():
+    return jsonify('{saluto:ciao}')
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
